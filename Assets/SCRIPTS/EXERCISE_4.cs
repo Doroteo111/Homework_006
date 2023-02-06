e65a46f31b588248d69dcaf46bc3cb9874c652e2
Doroteo111 <ladoritaesta@gmail.com>
2023-02-06 17:57:26 +0100

    quedan 4 a medias y 5 no
    el 4 son bombas para mi pc q esta pasando y el 5 es muy lol

--- Assets/SCRIPTS/EXERCISE_4.cs
+++ Assets/SCRIPTS/EXERCISE_4.cs
@@ -9,13 +9,41 @@ public class EXERCISE_4 : MonoBehaviour
     // For example, if start = 3 and end = 27, then in the console the numbers 5, 10, 15, 20 and 25 should be displayed
 
     /*public int start = 3;
-    public int end= 27;
-    void Start()
-    {
-        while (start <= end)
+   public int end= 27;
+   private int result;
+   public int value;
+   void Start()
+   {
+       while (start <= end)
+       {
+           Debug.Log(Multiples());
+
+       }
+
+   }
+
+   private bool Multiples()
+   {
+       result = value % 5;
+       if (result == 0)
+       {
+           return true;
+       }
+       else
+           return false;
+   }
+   //BUCLE INFINITO
+
+
+   /* {
+        while( start <= end)
         {
-            
+            Debug.Log(result);
+           result= start%5;
+        }
     }
-    */
+    */ //BUCLE INFINITO
+
+
 
 }
--- Assets/SCRIPTS/EXERCISE_5.cs
+++ Assets/SCRIPTS/EXERCISE_5.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EXERCISE_5 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Create a public int array. Create a private int variable. Compute the sum of the  elements of your array, store it in the
+    // private variable created earlier and display the result on the console.  or example, if your array is [1, 2, 3, 4, 5]
+    // , then the result displayed on the console will 1 + 2 + 3 + 4 + 5 = 15
+
+    public int[] numbers = { 1, 2, 3, 4, 5 };
+    private int otherNum;
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
