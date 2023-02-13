848d12793b52ca813afe5e6cc61762f8ebfa2851
Doroteo111 <ladoritaesta@gmail.com>
2023-02-13 10:46:39 +0100

    casi casi
    el 4 falta algo que no consigo

--- Assets/SCRIPTS/EXERCISE_4.cs
+++ Assets/SCRIPTS/EXERCISE_4.cs
@@ -8,42 +8,31 @@ public class EXERCISE_4 : MonoBehaviour
     // and end.Create a while loop that displays all multiples of 5 between start and end.
     // For example, if start = 3 and end = 27, then in the console the numbers 5, 10, 15, 20 and 25 should be displayed
 
-    /*public int start = 3;
+    public int start = 3;
    public int end= 27;
    private int result;
-   public int value;
+ 
    void Start()
    {
-       while (start <= end)
+        result = start;
+       while (result <= end) //arreglar y el paso comprobar elmnd
        {
-           Debug.Log(Multiples());
-
+          Multiples();
+            start++; 
        }
 
    }
 
-   private bool Multiples()
+   private void Multiples()
    {
-       result = value % 5;
-       if (result == 0)
+      
+       if (result % 5 == 0)
        {
-           return true;
+           Debug.Log(result);
        }
-       else
-           return false;
+       
    }
-   //BUCLE INFINITO
-
-
-   /* {
-        while( start <= end)
-        {
-            Debug.Log(result);
-           result= start%5;
-        }
-    }
-    */ //BUCLE INFINITO
-
+  
 
 
 }
